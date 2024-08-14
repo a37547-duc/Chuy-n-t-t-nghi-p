ee38e39b4f1500d663a33c0e06bad8a657b2a9cf
Phuc dz <ndbestyasuo@gmail.com>
2024-08-14 16:33:41 +0700

    update header

--- client/src/layouts/DefautLayout/DefauLayout.jsx
+++ client/src/layouts/DefautLayout/DefauLayout.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+
+import Home from "../../pages/Home/Home";
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
 
 export default function DefauLayout() {
   return (
     <div>
-      <Header>
-        <main>MAIN</main>
-      </Header>
+      <Header></Header>
+      <main>
+        <Home></Home>
+      </main>
       <Footer></Footer>
     </div>
   );
--- client/src/router.jsx
+++ client/src/router.jsx
@@ -2,19 +2,21 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import Root from "./pages/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home></Home>,
+    element: <Root></Root>,
 
-    // children: [
-    //   {
-    //     path: "team",
-    //     element: <Team />,
-    //     loader: teamLoader,
-    //   },
-    // ],
+    children: [
+      {
+        path: "team",
+        element: <Home></Home>,
+        index: true,
+        // loader: teamLoader,
+      },
+    ],
   },
 ]);
 
