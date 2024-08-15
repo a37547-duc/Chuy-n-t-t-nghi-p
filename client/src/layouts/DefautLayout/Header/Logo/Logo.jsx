611dad6511a8cfb6476d45763e1b4e50e24d5bb0
Phuc dz <ndbestyasuo@gmail.com>
2024-08-15 21:53:00 +0700

    modified header

--- client/src/layouts/DefautLayout/Header/Logo/Logo.jsx
+++ client/src/layouts/DefautLayout/Header/Logo/Logo.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Logo() {
   return <div className="ml-10">
-    <a href="#" class="flex items-center">
-      <img src="https://themewagon.com/wp-content/uploads/2021/03/Frame-172-1.png" class="mr-3 h-6 sm:h-9" alt="Computer logo" />
-      <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
+    <a href="#" className="flex items-center">
+      <img src="https://themewagon.com/wp-content/uploads/2021/03/Frame-172-1.png" className="mr-3 h-6 sm:h-9" alt="Computer logo" />
+      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white"></span>
     </a>
   </div>;
 }
