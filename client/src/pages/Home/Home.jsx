f3c416cd329c03f133f8480a6112fa26be423c7a
a37547-duc <anhtupeo1234@gmail.com>
2024-08-16 22:55:00 +0700

    Banner san pham

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Carousel from "./Carousel/Carousel";
 import BestSeller from "./BestSeller/BestSeller";
+import ProductCategory from "./ProductCategory/ProductCategory";
 
 export default function Home() {
   return (
     <div>
       <Carousel></Carousel>
       <BestSeller></BestSeller>
+      <ProductCategory></ProductCategory>
     </div>
   );
 }
---
+++ client/src/pages/Home/ProductCategory/ProductCategory.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { IoIosStar } from "react-icons/io";
+export default function ProductCategory() {
+  return (
+    <div className="bg-white-100 w-full min-h-screen gap-6 flex-wrap flex justify-center items-center">
+      {/* card1 */}
+      <div
+        className="w-60 p-2 bg-white rounded-xl transition-all hover:-translate-y-2  
+      duration-300 shadow-lg hover:shadow-2xl mt-4 mb-4 lg:mt-0"
+      >
+        <img
+          src="https://fastly.picsum.photos/id/268/200/300.jpg?hmac=M1JKzVXjrhIffE66T4sLediL7lhGmvS2rNr8pW2JipE"
+          alt=""
+          className="h-40 w-full object-cover rounded-xl"
+        />
+        <div className="p-2">
+          <h2 className="font-bold text-lg mb-2">Heading</h2>
+          <span className="text-xl font-semibold">180000</span>
+
+          <div className="flex items-center gap-2">
+            <span className="text-sm line-through opacity-75">Rs.20000</span>
+            <span className="font-bold text-sm p-2 bg-yellow-300 rounded-ss-2xl text-gray-600">
+              Save 10%
+            </span>
+          </div>
+
+          <div className="flex items-center mt-2 gap-1">
+            <IoIosStar className="text-yellow-300"></IoIosStar>
+            <IoIosStar className="text-yellow-300"></IoIosStar>
+            <IoIosStar className="text-yellow-300"></IoIosStar>
+            <IoIosStar className="text-yellow-300"></IoIosStar>
+            <p className="font-bold text-xs text-gray-700">Best Ratings</p>
+          </div>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+          </p>
+        </div>
+      </div>
+
+      {/* card1 */}
+    </div>
+  );
+}
