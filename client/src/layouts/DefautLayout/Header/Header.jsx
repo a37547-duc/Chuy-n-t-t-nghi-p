bc02675c2cbe7a0f824c39df4a874a91c85c6616
Phuc dz <ndbestyasuo@gmail.com>
2024-08-18 04:44:04 +0700

    update search bar

--- client/src/layouts/DefautLayout/Header/Header.jsx
+++ client/src/layouts/DefautLayout/Header/Header.jsx
@@ -8,11 +8,13 @@ export default function Header() {
   return (
     <header className="flex bg-teal-50 border-b py-4 sm:px-8 px-6 font-[sans-serif] min-h-[80px] tracking-wide relative z-50">
       <div className="flex flex-wrap items-center lg:gap-y-2 gap-4 w-full">
-        <Logo></Logo>
-        <SearchBar></SearchBar>
+        <Logo />
+        <div className="flex-grow">
+          <SearchBar className="w-full" />
+        </div>
         <div className="flex ml-auto gap-4 mr-20">
-          <AuthButtons></AuthButtons>
-          <Cart></Cart>
+          <AuthButtons />
+          <Cart />
         </div>
       </div>
     </header>
